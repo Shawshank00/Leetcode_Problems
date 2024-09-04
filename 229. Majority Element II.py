6685bea1eda8a33012c1a9d2995c2965e4294e23
Shashank Patel <112053940+Shawshank00@users.noreply.github.com>
2024-09-04 13:05:33 +0530

    Update 229. Majority Element II.py

--- 229. Majority Element II.py
+++ 229. Majority Element II.py
@@ -10,3 +10,7 @@ class Solution:
                 hashmap[i] = 1
 
         return [a for a,b in hashmap.items() if b > n]
+
+
+        #Time Complexity:O(n) where n is the number of elements in nums, due to single traversal of nums and hashmap items
+        #Space Complexity:O(n) for storing elements in the hashmap in the worst case
