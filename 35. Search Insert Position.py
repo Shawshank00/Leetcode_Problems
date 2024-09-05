66cd6108ac5cb9f003d56fc0e3a0237b08c68877
Shashank Patel <112053940+Shawshank00@users.noreply.github.com>
2024-09-05 12:51:19 +0530

    Update 35. Search Insert Position.py

--- 35. Search Insert Position.py
+++ 35. Search Insert Position.py
@@ -1,7 +1,30 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+       
+        '''
+        #Solution 1: Bruteforce
         n = 0
         for i in range(len(nums)):
             if nums[i] < target:
                 n += 1
         return n
+        #TC - O(n)
+        #SC - O(1)
+        '''
+        
+        #Solution 2: Optimal
+
+        L, R = 0, len(nums)
+
+        while L < R:
+            M = L + ((R-L) // 2)
+
+            if nums[M] < target:
+                L = M + 1
+            else:
+                R = M
+        return L
+
+        #TC - O(log n)
+        #SC - O(1)
+
