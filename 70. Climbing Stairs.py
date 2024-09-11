8f6ef905512e14b921f95816339bbbac3e76bcfa
Shashank Patel <112053940+Shawshank00@users.noreply.github.com>
2024-09-11 09:05:38 +0530

    Update 70. Climbing Stairs.py

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -1,7 +1,7 @@
+#1. using for loop: #O(n) & O(1)
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-
-        #This is fib series if visualized:
         prev1 = 2
         prev2 = 3
         count = 0
@@ -13,5 +13,29 @@ class Solution:
                 prev1 = prev2
                 prev2 = count
             return count
-                
-        
+
+#2. using Top down memorisation: #O(n) & O(n)
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {1:1, 2:2}
+        def f(n):
+            if n in memo:
+                return memo[n]
+            else:
+                memo[n] = f(n-1) + f(n-2)
+                return memo[n]
+        return f(n)
+
+#3. using Bottom up tabulation: #O(n) & O(n)
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        arr = [0] * n
+        arr[0] = 1
+        arr[1] = 2
+
+        for i in range(2,n):
+            arr[i] = arr[i-1] + arr[i-2]
+        return arr[n-1]
